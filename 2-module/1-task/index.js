c687885ef3c52bb9d331623fbe4c609b69b14534
Zakhapbk <ZZZ@zzz>
2023-12-07 11:49:11 +0300

    2 module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  rejectList = [NaN, Infinity, -Infinity]
+  let sum = 0
+  for (let i in salaries){
+    if (typeof salaries[i] === "number" && !rejectList.includes(salaries[i])){
+      sum += salaries[i]
+    }
+  }
+  return sum
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0
 };
-
+calculator.read = function (a,b){
+  this.a = a;
+  this.b = b
+}
+calculator.sum = function(){
+  return this.a + this.b
+}
+calculator.mul = function(){
+  return this.a * this.b
+}
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
