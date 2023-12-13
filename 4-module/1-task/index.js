e313ff848c14a67a1ea8887ff9bcaa96df40c661
Zakhapbk <ZZZ@zzz>
2023-12-13 03:28:05 +0300

    4 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let res = document.createElement('ul')
+  for (obj in friends){
+    let str = document.createElement('li')
+    str.innerHTML = `${friends[obj].firstName} ${friends[obj].lastName}`
+    res.appendChild(str)
+  }
+  return res
 }
