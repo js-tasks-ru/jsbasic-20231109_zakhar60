b2e0ac8d1ad0f844710cbf35c0a33f9cd948c2d9
Zakhapbk <ZZZ@zzz>
2023-12-13 17:25:08 +0300

    5 module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
+
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector(".toggle-text-button")
+  let text = document.getElementById("text")
+  button.addEventListener('click',function(){
+    if(!text.hasAttribute('hidden')){
+      text.setAttribute('hidden',true)
+    }
+    else{
+      text.removeAttribute('hidden')
+    }
+  })
+  
 }
